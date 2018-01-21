950d05811a999234e55ace5287f8f443c4a5469d
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-01-20 23:46:52 -0300

    teste

--- index.php
+++ index.php
@@ -33,7 +33,9 @@ if(isset($_SESSION["sent"]))
   <title>OTSB - Operation Tactics Snow Black</title>
 
   <!-- Bootstrap Core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+ <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+ <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
 
   <!-- Fonts -->
   <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
