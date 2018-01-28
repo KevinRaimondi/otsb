09284a822d63baf67ea1bcc846cc4e7de4854e5b
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-01-28 20:06:09 -0200

    teste

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ if(isset($_SESSION["sent"]))
           <li><a href="#jogos">Jogos</a></li>
           <li><a href="#sobre">Sobre</a></li>
           <li><a href="#contact">Contato</a></li>
-          <li><a href="/galeria#galeria">Galeria</a></li>
+          <li><a href="/galeria">Galeria</a></li>
           <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfKUjGVnpolEFRAuxI2Ati2C28M1Doij9uQtZPqpjyZr0J6ww/viewform">Aliste-se Já</a></li>
       <!--    <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
