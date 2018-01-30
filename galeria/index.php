451f23a55c1606077bb1b53776f81f4d3cc2199e
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-01-30 01:30:43 -0200

    GALERIA

---
+++ galeria/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>OTSB - Operation Tactics Snow Black</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+   <link rel="stylesheet" href="css/gridGallery.css" />
+
+  <!-- Fonts -->
+
+  <script src="https://use.fontawesome.com/0ca22a4c02.js"></script>
+  <link href="../css/animate.css" rel="stylesheet" />
+  <!-- Squad theme CSS -->
+  <link href="../css/style.css" rel="stylesheet">
+  <link href="../color/default.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: Squadfree
+    Theme URL: https://bootstrapmade.com/squadfree-free-bootstrap-template-creative/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+  <!-- Preloader -->
+  <div id="preloader">
+    <div id="load"></div>
+  </div>
+
+ <section id="galeria" class="nav-section">
+  <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header page-scroll">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button>
+        <a class="navbar-brand slogan" href="/">
+          <h1>OTSB</h1>
+        </a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="/">Home</a></li>
+          <li><a href="/#jogos">Jogos</a></li>
+          <li><a href="/#sobre">Sobre</a></li>
+          <li><a href="/#contact">Contato</a></li>
+          <li class="active"><a href="/galeria">Galeria</a></li>
+          <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSfKUjGVnpolEFRAuxI2Ati2C28M1Doij9uQtZPqpjyZr0J6ww/viewform">Aliste-se Já</a></li>
+      <!--    <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Example menu</a></li>
+              <li><a href="#">Example menu</a></li>
+              <li><a href="#">Example menu</a></li>
+            </ul>
+          </li-->
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+  </nav>
+
+</section>
+  <!-- Section: intro -->
+  <section class="logo galeria-section">
+
+    <div id="grid" data-directory="gallery"></div>
+
+    <!-- SCRIPTS FOR FOR THE PLUGIN-->
+    <script src="js/jquery-1.9.1.min.js"></script>
+    <script src="js/rotate-patch.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/autoGrid.min.js"></script>
+
+    <script>
+      $(function(){
+            //INITIALIZE THE PLUGIN
+            $('#grid').grid({
+                categoriesOrder: 'byName', //byDate, byDateReverse, byName, byNameReverse, random
+                imagesOrder: 'byDate', //byDate, byDateReverse, byName, byNameReverse, random
+                isFitWidth: true, //Nedded to be true if you wish to center the gallery to its container
+                lazyLoad: true, //If you wish to load more images when it reach the bottom of the page
+                showNavBar: true, //Show the navigation bar?
+                smartNavBar: true, //Hide the navigation bar when you don't have categories or just 1
+                imagesToLoadStart: 15, //The number of images to load when it first loads the grid
+                imagesToLoad: 5, //The number of images to load when you click the load more button
+                aleatoryImagesFromCategories: true,//Get few images from each category if not it will get them in order
+                horizontalSpaceBetweenThumbnails: 15, //The space between images horizontally
+                verticalSpaceBetweenThumbnails: 15, //The space between images vertically
+                columnWidth: 'auto', //The width of each columns, if you set it to 'auto' it will use the columns instead
+                columns: 5, //The number of columns when you set columnWidth to 'auto'
+                columnMinWidth: 220, //The minimum width of each columns when you set columnWidth to 'auto'
+                isAnimated: true, //Animation when resizing or filtering with the nav bar
+                caption: true, //Show the caption in mouse over
+                captionCategory: true,//Show the category section of the caption
+                captionType: 'grid', // 'grid', 'grid-fade', 'classic' the type of caption effect
+                lightBox: true, //Do you want the lightbox?
+                lightboxKeyboardNav: true, //Keyboard navigation of the next and prev image
+                lightBoxSpeedFx: 500, //The speed of the lightbox effects
+                lightBoxZoomAnim: true, //Do you want the zoom effect of the images in the lightbox?
+                lightBoxText: true, //If you wish to show the text in the lightbox
+                lightboxPlayBtn: true, //Show the play button?
+                lightBoxAutoPlay: false, //The first time you open the lightbox it start playing the images
+                lightBoxPlayInterval: 4000, //The interval in the auto play mode 
+                lightBoxShowTimer: true, //If you wish to show the timer in auto play mode
+                lightBoxStopPlayOnClose: false, //Stop the auto play mode when you close the lightbox?
+            });
+      });
+    </script>
+
+  </section>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-12">
+          <div class="wow shake" data-wow-delay="0.4s">
+            <div class="page-scroll marginbot-30">
+              <a href="#galeria" id="totop" class="btn btn-circle">
+							<i class="fa fa-angle-double-up animated"></i>
+						</a>
+            </div>
+          </div>
+          <p>&copy;SquadFREE. All rights reserved.</p>
+          <div class="credits">
+            <!--
+              All the links in the footer should remain intact. 
+              You can delete the links only if you purchased the pro version.
+              Licensing information: https://bootstrapmade.com/license/
+              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Squadfree
+            -->
+            <a href="https://bootstrapmade.com/bootstrap-one-page-templates/">Bootstrap One Page Templates</a> by BootstrapMade
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Core JavaScript Files -->
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/wow.min.js"></script>
+
+  <!-- Custom Theme JavaScript -->
+  <script src="../js/custom.js"></script>
+
+</body>
+
+</html>
