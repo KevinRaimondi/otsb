75c4193d3a23f77d6ad078dc1f0f9af34070b924
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-09 22:05:04 -0200

    Test LOAD

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ if(isset($_SESSION['cmsg'])){
     <div class="slogan">
       <h2>BEM-VINDOS</h2>
       <h4>OPERATION TACTICS SNOW BLACK</h4>
-      <i class="fas fa-circle-notch"></i>
+      <i class="fa fa-circle-o-notch fa-spin fa-4x fa-fw margin-bottom"></i>
     </div>
     <div class="page-scroll">
       <a href="#jogos" class="btn btn-circle">
