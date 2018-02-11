51d788d81d2bf1e80b46160975eb07adfa5ae4cc
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-11 16:00:00 -0200

    Criando cadastro de usuario

--- cadastro.php
+++ cadastro.php
@@ -17,55 +17,73 @@ if (isset($_POST['cadastrar'])) {
 	if (!empty($foto["name"])) {
 		
 		// Largura máxima em pixels
-		$largura = 251;
+		$largura = 151;
 		// Altura máxima em pixels
-		$altura = 251;
+		$altura = 151;
 		// Tamanho máximo do arquivo em bytes
 		$tamanho = 100000;
 
 		$error = array();
 
     	// Verifica se o arquivo é uma imagem
-    	if(!preg_match("/^image\/(pjpeg|jpeg|png|gif|bmp)$/", $foto["type"])){
-     	   $error[1] = "Isso não é uma imagem.";
-   	 	} 
-	
+		if(!preg_match("/^image\/(pjpeg|jpeg|png|gif|bmp)$/", $foto["type"])){
+			$error[1] = "Isso não é uma imagem.";
+		} 
+
 		// Pega as dimensões da imagem
 		$dimensoes = getimagesize($foto["tmp_name"]);
-	
-		// Verifica se a largura da imagem é maior que a largura permitida
-		if($dimensoes[0] > $largura) {
-			$error[2] = "A largura da imagem não deve ultrapassar ".$largura." pixels";
-		}
 
-		// Verifica se a altura da imagem é maior que a altura permitida
-		if($dimensoes[1] > $altura) {
-			$error[3] = "Altura da imagem não deve ultrapassar ".$altura." pixels";
-		}
-		
 		// Verifica se o tamanho da imagem é maior que o tamanho permitido
 		if($foto["size"] > $tamanho) {
-   		 	$error[4] = "A imagem deve ter no máximo ".$tamanho." bytes";
+			$error[4] = "A imagem deve ter no máximo ".$tamanho." bytes";
 		}
+
+		   // Calculando a proporção
+		$ratio_orig = $dimensoes[1]/$dimensoes[0];
+
+		if ($width/$height > $ratio_orig) {
+			$width = $height*$ratio_orig;
+		} else {
+			$height = $width/$ratio_orig;
+		}
+
+		$image_p = imagecreatetruecolor($width, $height);
+		$image = imagecreatefromjpeg($filename);
+		imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+
+		$foto = imagejpeg($image_p, $foto["name"], 75);
+
+
+
+		// Verifica se a largura da imagem é maior que a largura permitida
+		//if($dimensoes[0] > $largura) {
+		//	$error[2] = "A largura da imagem não deve ultrapassar ".$largura." pixels";
+		//}
+
+		// Verifica se a altura da imagem é maior que a altura permitida
+		//if($dimensoes[1] > $altura) {
+		//	$error[3] = "Altura da imagem não deve ultrapassar ".$altura." pixels";
+		//}
+		
 
 		// Se não houver nenhum erro
 		if (count($error) == 0) {
-		
+
 			// Pega extensão da imagem
 			preg_match("/\.(gif|bmp|png|jpg|jpeg){1}$/i", $foto["name"], $ext);
 
         	// Gera um nome único para a imagem
-        	$nome_imagem = md5(uniqid(time())) . "." . $ext[1];
+			$nome_imagem = md5(uniqid(time())) . "." . $ext[1];
 
         	// Caminho de onde ficará a imagem
-        	$caminho_imagem = $path . $nome_imagem;
+			$caminho_imagem = $path . $nome_imagem;
 
 			// Faz o upload da imagem para seu respectivo caminho
 			move_uploaded_file($foto["tmp_name"], $caminho_imagem);
-		
+
 			// Insere os dados no banco
 			$sql = mysqli_query($conn, "INSERT INTO usuarios VALUES ('', '".$nome."', '".$email."', '".$nome_imagem."')");
-		
+
 			// Se os dados forem inseridos com sucesso
 			if ($sql){
 				echo "Você foi cadastrado com sucesso.";
@@ -74,7 +92,7 @@ if (isset($_POST['cadastrar'])) {
 				echo $caminho_imagem;
 			}
 		}
-	
+
 		// Se houver mensagens de erro, exibe-as
 		if (count($error) != 0) {
 			foreach ($error as $erro) {
@@ -87,42 +105,42 @@ if (isset($_POST['cadastrar'])) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Cadastro de usuário</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Cadastro de usuário</title>
 </head>
 
 <body>
-<h1>Novo Usuário</h1>
-<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data" name="cadastro" >
-Nome:<br />
-<input type="text" name="nome" /><br /><br />
-Email:<br />
-<input type="text" name="email" /><br /><br />
-Senha:<br />
-<input type="password" name="senha" /><br /><br />
-Foto de exibição:<br />
+	<h1>Novo Usuário</h1>
+	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data" name="cadastro" >
+		Nome:<br />
+		<input type="text" name="nome" /><br /><br />
+		Email:<br />
+		<input type="text" name="email" /><br /><br />
+		Senha:<br />
+		<input type="password" name="senha" /><br /><br />
+		Foto de exibição:<br />
 
-<input type="file" name="foto" accept="image/jpeg" /><br /><br />
-<input type="submit" name="cadastrar" value="Cadastrar" />
-</form>
+		<input type="file" name="foto" accept="image/jpeg" /><br /><br />
+		<input type="submit" name="cadastrar" value="Cadastrar" />
+	</form>
 
-<hr />
+	<hr />
 
-<h1>Usuários cadastrados</h1>
-<?php
+	<h1>Usuários cadastrados</h1>
+	<?php
 // Seleciona todos os usuários
-$sql = mysqli_query($conn, "SELECT * FROM usuarios ORDER BY nome");
+	$sql = mysqli_query($conn, "SELECT * FROM usuarios ORDER BY nome");
 
 // Exibe as informações de cada usuário
-while ($usuario = mysqli_fetch_object($sql)) {
+	while ($usuario = mysqli_fetch_object($sql)) {
 	// Exibimos a foto
-	echo "<img src='".$path.$usuario->foto."' alt='Foto de exibição' /><br />";
+		echo "<img src='".$path.$usuario->foto."' alt='Foto de exibição' /><br />";
 	// Exibimos o nome e email
-	echo "<b>Nome:</b> " . $usuario->nome . "<br />";
-	echo "<b>Email:</b> " . $usuario->email . "<br /><br />";
-}
+		echo "<b>Nome:</b> " . $usuario->nome . "<br />";
+		echo "<b>Email:</b> " . $usuario->email . "<br /><br />";
+	}
 
 
-?>
+	?>
 </body>
 </html>
