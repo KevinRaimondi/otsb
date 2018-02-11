565d80b4e22c59c5c95a29a312f5985cbecc574b
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-11 15:43:59 -0200

    Criando cadastro de usuario

--- cadastro.php
+++ cadastro.php
@@ -116,7 +116,7 @@ $sql = mysqli_query($conn, "SELECT * FROM usuarios ORDER BY nome");
 // Exibe as informações de cada usuário
 while ($usuario = mysqli_fetch_object($sql)) {
 	// Exibimos a foto
-	echo "<img src='"$path.$usuario->foto."' alt='Foto de exibição' /><br />";
+	echo "<img src='".$path.$usuario->foto."' alt='Foto de exibição' /><br />";
 	// Exibimos o nome e email
 	echo "<b>Nome:</b> " . $usuario->nome . "<br />";
 	echo "<b>Email:</b> " . $usuario->email . "<br /><br />";
