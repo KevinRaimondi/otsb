ffc85f464cf685543903c7379dee1366d43a15e3
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-11 15:38:06 -0200

    Criando cadastro de usuario

--- cadastro.php
+++ cadastro.php
@@ -64,7 +64,7 @@ if (isset($_POST['cadastrar'])) {
 			move_uploaded_file($foto["tmp_name"], $caminho_imagem);
 		
 			// Insere os dados no banco
-			$sql = mysql_query("INSERT INTO usuarios VALUES ('', '".$nome."', '".$email."', '".$nome_imagem."')");
+			$sql = mysqli_query("INSERT INTO usuarios VALUES ('', '".$nome."', '".$email."', '".$nome_imagem."')");
 		
 			// Se os dados forem inseridos com sucesso
 			if ($sql){
@@ -115,7 +115,7 @@ $sql = mysqli_query($conn, "SELECT * FROM usuarios ORDER BY nome");
 // Exibe as informações de cada usuário
 while ($usuario = mysqli_fetch_object($sql)) {
 	// Exibimos a foto
-	echo "<img src='fotos/".$usuario->foto."' alt='Foto de exibição' /><br />";
+	echo "<img src='"$path"/".$usuario->foto."' alt='Foto de exibição' /><br />";
 	// Exibimos o nome e email
 	echo "<b>Nome:</b> " . $usuario->nome . "<br />";
 	echo "<b>Email:</b> " . $usuario->email . "<br /><br />";
