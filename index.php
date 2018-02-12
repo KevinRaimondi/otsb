19b266858a5c544e4e76e602baed7aba7aa715d4
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-12 00:36:03 -0200

    ifancybox

--- index.php
+++ index.php
@@ -574,12 +574,15 @@ if(isset($_SESSION['cmsg'])){
     </div>
   </footer>
 
-  <!-- Core JavaScript Files -->
-
-  <script src="js/wow.min.js"></script>
-  <!-- Custom Theme JavaScript -->
-  <script src="js/custom.js"></script>
-  <script src="contactform/contactform.js"></script>
+<!-- Core JavaScript Files -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/jquery.scrollTo.js"></script>
+<script src="js/wow.min.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="js/custom.js"></script>
+<script src="contactform/contactform.js"></script>
 
 </body>
 
