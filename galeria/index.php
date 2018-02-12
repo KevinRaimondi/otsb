4cef4605278bbee4a952236ea4f04ea6bba4664e
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-12 01:16:12 -0200

    ifancybox

--- galeria/index.php
+++ galeria/index.php
@@ -164,12 +164,12 @@
   </footer>
 
   <!-- Core JavaScript Files -->
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery.easing.min.js"></script>
-  <script src="js/jquery.scrollTo.js"></script>
-  <script src="js/wow.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/jquery.easing.min.js"></script>
+  <script src="../js/jquery.scrollTo.js"></script>
+  <script src="../js/wow.min.js"></script>
   <!-- Custom Theme JavaScript -->
-  <script src="js/custom.js"></script>
+  <script src="../js/custom.js"></script>
 
 </body>
 
