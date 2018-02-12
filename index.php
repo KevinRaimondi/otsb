8367058abf96e75abca68714936499969421da78
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-12 00:21:07 -0200

    ifancybox

--- index.php
+++ index.php
@@ -44,24 +44,23 @@ if(isset($_SESSION['cmsg'])){
   <!-- Squad theme CSS -->
   <link href="css/style.css" rel="stylesheet">
   <link href="color/default.css" rel="stylesheet">
-
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
   <script>
     !window.jQuery && document.write('<script src="js/jquery-1.4.3.min.js"><\/script>');
-    <script type="text/javascript" src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-    <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.4.css" media="screen" />
-    <script type="text/javascript">
-      $(document).ready(function(){
-        $(".ifancybox").fancybox({
-          'width' : '75%',
-          'height' : '75%',
-          'autoScale' : false,
-          'transitionIn' : 'none',
-          'transitionOut' : 'none',
-          'type' : 'iframe'
-        });
-      });
-    </script>
+  </script>
+  <script type="text/javascript" src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+  <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+  <script type="text/javascript">
+    $(document).ready(function(){
+    $(".ifancybox").fancybox({
+      'width' : '75%',
+      'height' : '75%',
+      'autoScale' : false,
+      'transitionIn' : 'none',
+      'transitionOut' : 'none',
+      'type' : 'iframe'
+    });
+  });
+  </script>
 
 
 
@@ -574,6 +573,7 @@ if(isset($_SESSION['cmsg'])){
   </footer>
 
   <!-- Core JavaScript Files -->
+  <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/jquery.easing.min.js"></script>
   <script src="js/jquery.scrollTo.js"></script>
