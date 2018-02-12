44f138416155e3cbfecb30fa2121de4d7a353bcd
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-12 19:47:50 -0300

    AJUSTE NO FOMR DE CADASTRO

--- galeria/cadastro.php
+++ galeria/cadastro.php
@@ -249,21 +249,17 @@ function inserir($conn, $usuario, $email, $senha, $nome_imagem, $caminho_imagem,
       <div class="col-lg-8">
         <div class="panel panel-default" >
           <div class="panel-heading">
-            <div class="panel-title text-center">Bootsnipp.com</div>
+            <div class="panel-title text-center">Cadastro</div>
           </div>     
 
           <div class="panel-body" >
 
             <form id="form" class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data" name="cadastro">
-              <fieldset>
 
-                <!-- Form Name -->
-                <legend>Cadastro</legend>
                 <table class="align-center width-100">
                   <tr>
                     <td colspan="3" style="width: 49%;">
                       <!-- Text input-->
-                      <label class="control-label" for="usuario"><span style="color: red;">*</span> Usuário:</label>  
                       <div class="input-group width-100">
                         <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
                         <input id="usuario" name="usuario" type="text" placeholder="Usuário" class="form-control input-md" value="<?php echo $usuario ?>" required="">
@@ -274,7 +270,6 @@ function inserir($conn, $usuario, $email, $senha, $nome_imagem, $caminho_imagem,
 
                     <!-- Prepended text-->
                     <td style="width: 49%;">
-                      <label class="control-label" for=""><span style="color: red;">*</span> E-mail:</label>
                       <div class="input-group width-100">
                         <span class="input-group-addon"><i class="fa fa-envelope" aria-hidden="true"></i></span>
                         <input id="email" name="email" class="form-control" placeholder="E-mail" type="email" value="<?php echo $email ?>" required="">
@@ -284,8 +279,7 @@ function inserir($conn, $usuario, $email, $senha, $nome_imagem, $caminho_imagem,
 
                   <!-- Password input-->
                   <tr>
-                    <td colspan="3" style="width: 49%;">
-                      <label class="control-label" for="passwordinput"><span style="color: red;">*</span> Senha:</label>
+                    <td colspan="3" style="width: 49%; height: 65px;">
                       <div class="input-group width-100" style="display: flex;">
                         <input id="passwordinput" name="passwordinput" type="password" placeholder="Informe sua senha" class="form-control input-md" required="">
                         <button id="btnPasswordinput" class="input-group-addon fa fa-eye" type="button" style="width: 11%;"></button>
@@ -295,7 +289,6 @@ function inserir($conn, $usuario, $email, $senha, $nome_imagem, $caminho_imagem,
                     <td style="width: 2%;"/>
 
                     <td colspan="3" style="width: 49%;">
-                      <label class="control-label" for="passwordinputConfirm"><span style="color: red;">*</span> Confirme sua senha:</label>
                       <div class="input-group width-100" style="display: flex;">
                         <input id="passwordinputConfirm" name="passwordinputConfirm" type="password" placeholder="Confirme sua senha" class="form-control input-md" required="">
                         <button id="btnPasswordinputConfirm" class="input-group-addon fa fa-eye" type="button" style="width: 11%;"></button>
@@ -305,8 +298,7 @@ function inserir($conn, $usuario, $email, $senha, $nome_imagem, $caminho_imagem,
 
                   <!-- File Button --> 
                   <tr>
-                    <td colspan="3" style="width: 49%;">
-                      <label class="control-label" for="filebutton">Foto do perfil (150 X 150):</label>
+                    <td colspan="3" style="width: 49%; height: 65px;">
                       <input id="filebutton" name="file-input" class="input-file width-100" type="file" accept="image/jpeg">
                     </td>
 
@@ -314,7 +306,6 @@ function inserir($conn, $usuario, $email, $senha, $nome_imagem, $caminho_imagem,
 
                     <td colspan="3" style="width: 49%;">
                       <!-- Text input-->
-                      <label class="control-label" for="token"><span style="color: red;">*</span> Token:</label>
                       <div class="input-group width-100">
                         <span class="input-group-addon" style="width: 11%;"><i class="fa fa-lock" aria-hidden="true"></i></span>
                         <input id="token" name="token" type="text" placeholder="Informe seu Token" class="form-control input-md" required="">
@@ -338,7 +329,6 @@ function inserir($conn, $usuario, $email, $senha, $nome_imagem, $caminho_imagem,
                 </tr>
               </table>
 
-            </fieldset>
           </form>
         </div>                     
 
