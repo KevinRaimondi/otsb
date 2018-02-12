ded84e96e33f88b7621f0fed383694fc1b0255b2
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-12 01:01:17 -0200

    ifancybox

--- galeria/index.php
+++ galeria/index.php
@@ -22,16 +22,13 @@
   <link href="../color/default.css" rel="stylesheet">
 
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-  <script>
-    !window.jQuery && document.write('<script src="jquery-1.4.3.min.js"><\/script>');
-  </script>
-  <script type="text/javascript" src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-  <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+  <script type="text/javascript" src="../fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+  <link rel="stylesheet" type="text/css" href="../fancybox/jquery.fancybox-1.3.4.css" media="screen" />
   <script type="text/javascript">
     $(document).ready(function(){
       $(".ifancybox").fancybox({
-        'width' : '85%',
-        'height' : '85%',
+        'width' : '95%',
+        'height' : '95%',
         'autoScale' : false,
         'transitionIn' : 'none',
         'transitionOut' : 'none',
