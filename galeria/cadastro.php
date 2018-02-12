b4305a661576cefb3301abc70e95b6ac6e1bbc26
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-11 22:15:25 -0200

    Criando cadastro de usuario

--- galeria/cadastro.php
+++ galeria/cadastro.php
@@ -127,7 +127,7 @@ function inserir($conn, $usuario, $email, $senha, $nome_imagem, $caminho_imagem)
   $msg = "";
 
   // Insere os dados no banco
-  $sql = mysqli_query($conn, "INSERT INTO usuarios VALUES ('', '".$usuario."', '".$email."', '".$senha."', '".$nome_imagem."')");
+  $sql = mysqli_query($conn, "INSERT INTO usuarios VALUES ('kk', '".$usuario."', '".$email."', '".$senha."', '".$nome_imagem."')");
 
   // Se os dados forem inseridos com sucesso
   if ($sql){
