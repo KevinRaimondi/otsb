0d120c01cd7cb796f8bbedc4e5cc069b065a39e0
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-14 00:27:20 -0300

    Iniciando Construção do painel

---
+++ painel/index.php
@@ -0,0 +1,152 @@
+<?php
+// Conexão com o banco de dados
+require ("../conexao.php");
+$msg = '';
+$email = "";
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>OTSB - Operation Tactics Snow Black</title>
+
+  <!-- Bootstrap Core CSS -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Fonts -->
+
+  <script src="https://use.fontawesome.com/0ca22a4c02.js"></script>
+  <link href="../css/animate.css" rel="stylesheet" />
+  <!-- Squad theme CSS -->
+  <link href="../css/style.css" rel="stylesheet">
+  <link href="../color/default.css" rel="stylesheet">
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+  <script type="text/javascript" src="../fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+  <link rel="stylesheet" type="text/css" href="../fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $(".ifancybox").fancybox({
+        'width' : '95%',
+        'height' : '95%',
+        'autoScale' : false,
+        'transitionIn' : 'none',
+        'transitionOut' : 'none',
+        'type' : 'iframe'
+      });
+    });
+  </script>
+
+  <style type="text/css">
+  a{
+    color: white;
+  }
+</style>
+
+  <!-- =======================================================
+  Theme Name: Squadfree
+  Theme URL: https://bootstrapmade.com/squadfree-free-bootstrap-template-creative/
+  Author: BootstrapMade
+  Author URL: https://bootstrapmade.com
+  ======================================================= -->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+  <!-- Preloader -->
+  <div id="preloader">
+    <div id="load">
+      <i class="fa fa-circle-o-notch fa-spin fa-4x fa-fw margin-bottom"></i>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header page-scroll">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+          <i class="fa fa-bars"></i>
+        </button>
+        <a class="navbar-brand slogan" href="/">
+          <h1>OTSB</h1>
+        </a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+
+      <div class="collapse navbar-collapse navbar-right navbar-main-collapse" style="padding-left: 10px;">
+        <ul class="nav navbar-nav">
+          <li><a href="/login"><i class="fa fa-sign-in" aria-hidden="true"></i> Entrar</a></li>
+        </ul>
+      </div>
+
+      <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="/">Home</a></li>
+          <li><a href="/#jogos">Jogos</a></li>
+          <li><a href="/#sobre">Sobre</a></li>
+          <li><a href="/#contact">Contato</a></li>
+          <li><a href="/galeria">Galeria</a></li>
+          <li><a class="ifancybox" href="https://docs.google.com/forms/d/e/1FAIpQLSfKUjGVnpolEFRAuxI2Ati2C28M1Doij9uQtZPqpjyZr0J6ww/viewform">Aliste-se Já</a></li>
+          <!--<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+          <li><a href="#">Example menu</a></li>
+          <li><a href="#">Example menu</a></li>
+          <li><a href="#">Example menu</a></li>
+          </ul>
+        </li-->
+      </ul>
+    </div>
+    <!-- /.navbar-collapse -->
+  </div>
+  <!-- /.container -->
+</nav>
+
+<!-- Section: intro -->
+<section id="intro" class="intro">
+
+  <div class="container">
+    <div class="row centralizar-divs">
+      <div class="col-lg-4">
+        <div class="panel panel-default" >
+          <div class="panel-heading">
+            <div class="panel-title text-center">Recupere sua senha</div>
+          </div>     
+
+          <ul class="nav nav-tabs"> 
+            
+            <li class="active"><a href="#tab1default" data-toggle="tab">Meu Perfil</a></li>
+            <li><a href="#tab2default" data-toggle="tab">Minhas Fotos</a></li>
+            <li><a href="#tab3default" data-toggle="tab">Enviar Fotos</a></li>
+          </ul>
+        </div>
+        <div class="panel-body">
+          <div class="tab-content">
+            <div class="tab-pane fade in active" id="tab1default">Default 1</div>
+            <div class="tab-pane fade" id="tab2default">Default 2</div>
+            <div class="tab-pane fade" id="tab3default">Default 3</div>
+          </div>                 
+        </div>  
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<!-- Core JavaScript Files -->
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/jquery.easing.min.js"></script>
+<script src="../js/jquery.scrollTo.js"></script>
+<script src="../js/wow.min.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="../js/custom.js"></script>
+
+</body>
+
+</html>
