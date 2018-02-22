492b04fc613542758a2c2e4771443f6dc1717860
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-22 15:12:58 -0300

    SQL Atualizado

--- u163043632_otsb.sql
+++ u163043632_otsb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 13/02/2018 às 03:58
+-- Tempo de geração: 22/02/2018 às 18:11
 -- Versão do servidor: 10.1.29-MariaDB
 -- Versão do PHP: 7.0.25
 
@@ -25,6 +25,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `log`
+--
+
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `descricao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `tokens`
 --
 
@@ -32,6 +44,14 @@ CREATE TABLE `tokens` (
   `id` int(11) NOT NULL,
   `token` varchar(100) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Fazendo dump de dados para tabela `tokens`
+--
+
+INSERT INTO `tokens` (`id`, `token`) VALUES
+(17, 'tctvbnhj45m4k'),
+(18, '567854iorekterfe');
 
 -- --------------------------------------------------------
 
@@ -44,20 +64,28 @@ CREATE TABLE `usuarios` (
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `senha` varchar(100) NOT NULL,
-  `foto` varchar(100) NOT NULL
+  `foto` varchar(100) NOT NULL,
+  `permissao` int(1) NOT NULL DEFAULT '0',
+  `status` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Fazendo dump de dados para tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `foto`) VALUES
-(8, 'faael', 'faelcalves@hotmail.com', '', '2dd945d3c0471656ce5f0a4bb587bcbf.jpg'),
-(46, 'fabiolucenaribas', 'fabio.luce@outlook.com', '872511fff87623280799ba124a0847f5', 'cbdfec6d3f86516e2490ea43c72a21d2.jpg');
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `foto`, `permissao`, `status`) VALUES
+(8, 'faael', '', '', '2dd945d3c0471656ce5f0a4bb587bcbf.jpg', 0, 1),
+(46, 'fabiolucenaribas', 'fabio.luce@outlook.com', '76e3e53ee06d9bfdd7f26c36e0633de7', 'cbdfec6d3f86516e2490ea43c72a21d2.jpg', 1, 2);
 
 --
 -- Índices de tabelas apagadas
 --
+
+--
+-- Índices de tabela `log`
+--
+ALTER TABLE `log`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `tokens`
@@ -76,16 +104,22 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `log`
+--
+ALTER TABLE `log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
