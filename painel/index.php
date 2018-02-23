704439207732e895ba8536c41f39c018970dbfb9
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-22 21:20:29 -0300

    Iniciando Construção do painel

--- painel/index.php
+++ painel/index.php
@@ -143,39 +143,40 @@ if(!isset($_SESSION["login"]) || !isset($_SESSION["senha"])){
                   <section class="push-top-sm hr">
                     <?php  echo "<h4 class='text-center'>".$dados['nome']."</h4>";  ?>
                   </section>
-                  <ul class="nav">
-                    <li class="active"><a href="#tab1default1" data-toggle="tab"><i class="fa fa-info-circle" aria-hidden="true"></i> Informações do perfil</a></li>
-                    <li ><a href="#tab1default2" data-toggle="tab"><i class="fa fa-edit" aria-hidden="true"></i> Editar perfil</a></li>
-                    <li ><a href="#tab1default3" data-toggle="tab"><i class="fa fa-image" aria-hidden="true"></i> Imagem do perfil</a></li>
-                  </ul>
+                  <div class="panel with-nav-tabs panel-default">
+                    <ul class="nav">
+                      <li class="active"><a href="#tab1default1" data-toggle="tab"><i class="fa fa-info-circle" aria-hidden="true"></i> Informações do perfil</a></li>
+                      <li ><a href="#tab1default2" data-toggle="tab"><i class="fa fa-edit" aria-hidden="true"></i> Editar perfil</a></li>
+                      <li ><a href="#tab1default3" data-toggle="tab"><i class="fa fa-image" aria-hidden="true"></i> Imagem do perfil</a></li>
+                    </ul>
+                  </div>
                 </section>
 
-                <div class="panel with-nav-tabs panel-default">
-                  <div class="tab-content">
-                    <div id="tab1default1" class="tab-pane fade in active">
-                      <section class="block-content panel-margin profile-classic">
-                        <ul class="nav">
-                         <?php  echo "<li><span>Usuário:</span> ".$dados['nome']."</li>";?>
-                         <?php  echo "<li><span><span><i class='fa fa-envelope'></i> Email: <a href='mailto:".$dados['email']."'>".$dados['email']."</a></span></li>"; ?>
-                         <li><span>Estado:</span> Ativo</li>
-                         <li><span>Membro desde:</span> 01/12/2017</li>
-                       </ul>
-                     </section>
-                   </div>
-
-                   <div id="tab1default2" class="tab-pane fade">
+                <div class="tab-content">
+                  <div id="tab1default1" class="tab-pane fade in active">
                     <section class="block-content panel-margin profile-classic">
-                      <span>Editar Perfil</span>
-                    </section>
-                  </div>
+                      <ul class="nav">
+                       <?php  echo "<li><span>Usuário:</span> ".$dados['nome']."</li>";?>
+                       <?php  echo "<li><span><span><i class='fa fa-envelope'></i> Email: <a href='mailto:".$dados['email']."'>".$dados['email']."</a></span></li>"; ?>
+                       <li><span>Estado:</span> Ativo</li>
+                       <li><span>Membro desde:</span> 01/12/2017</li>
+                     </ul>
+                   </section>
+                 </div>
 
-                  <div id="tab1default3" class="tab-pane fade">
-                    <section class="block-content panel-margin profile-classic">
-                      <span>Imagem do perfil</span>
-                    </section>
-                  </div>
+                 <div id="tab1default2" class="tab-pane fade">
+                  <section class="block-content panel-margin profile-classic">
+                    <span>Editar Perfil</span>
+                  </section>
+                </div>
+
+                <div id="tab1default3" class="tab-pane fade">
+                  <section class="block-content panel-margin profile-classic">
+                    <span>Imagem do perfil</span>
+                  </section>
                 </div>
               </div>
+
 
             </div>
             <div class="tab-pane fade" id="tab2default">Default 2</div>
