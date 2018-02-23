76b182da3479e3dc02657d5ca6f4a6d1f3afee37
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-23 14:18:47 -0300

    Iniciando Construção do painel - meu perfil

--- painel/index.php
+++ painel/index.php
@@ -3,6 +3,7 @@
 require ("../conexao.php");
 $permissao = 0;
 $path = "../img/profile/";
+$msg = '';
 
 session_start();
 
@@ -10,6 +11,7 @@ if(!isset($_SESSION["login"]) || !isset($_SESSION["senha"])){
   header ("Location: /login");
 }else{
 
+  $id = $_SESSION["id"]
   $email = $_SESSION["login"];
   $senha = $_SESSION["senha"];
 
@@ -18,44 +20,90 @@ if(!isset($_SESSION["login"]) || !isset($_SESSION["senha"])){
   $permissao =  $dados['permissao'];
 }
 
+if (isset($_POST['btnAtualizar'])) {
+
+  $emailNovo = $_POST['email'];
+  $senhaNova = $_POST['passwordinput'];
+  $senhaNovaConfim = $_POST['passwordinputConfirm'];
+  $senhaAtual = $_POST['passwordAtual'];
+  $enrypt = md5($senhaAtual);
+  $enryptSenhaNova = md5($senhaNova);
+
+  if($senha == $enrypt){
+    if((!empty($senhaNova) || !empty($senhaNovaConfim)) && (!empty($senhaNova) && empty($senhaNovaConfim)) || (!empty($senhaNovaConfim) && empty($senhaNova))){ 
+      $msg = "<p id='mensagem' style='text-shadow: 0px 0px 5px #f00; margin: 0;'>As senhas não conferem!</p>";
+    }else if($senha != $senhaConfim){
+      $msg = "<p id='mensagem' style='text-shadow: 0px 0px 5px #f00; margin: 0;'>As senhas não conferem!</p>";
+    }else if (strlen($senha) < 8) {
+      $msg = "<p id='mensagem' style='text-shadow: 0px 0px 5px #f00; margin: 0;'>Senha com no minimo 8 caracteres!</p>";
+    }else{
+      if($email != $emailNovo){
+        $sql = mysqli_query($conn, "UPDATE `usuarios` SET `email` = '"$emailNovo"', `senha` = '".$enryptSenhaNova."' WHERE `usuarios`.`id` = '".$id."'");
+      }else{
+        $sql = mysqli_query($conn, "UPDATE `usuarios` SET `senha` = '".$enryptSenhaNova."' WHERE `usuarios`.`id` = '".$id."'");
+      }
+      if ($sql){
+        $msg = "<p id='mensagem' style='text-shadow: 0px 0px 5px #000; margin: 0;'>Informações atualizadas com sucesso.</p>";
+      }else{
+        $msg = "<p id='mensagem' style='text-shadow: 0px 0px 5px #f00; margin: 0;'>Erro ao tualizar! Favor entra em contato com o administrador.</p>";
+      }
+    }
+  }else{
+   $msg = "<p id='mensagem' style='text-shadow: 0px 0px 5px #f00; margin: 0;'>Senha atual não confere!</p>";
+ }
+}
+
+
+
+if (isset($_POST['btnAtualizarFoto'])) {
+
+}
+
+
+
+
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="">
-  <meta name="author" content="">
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="description" content="">
+ <meta name="author" content="">
 
-  <title>OTSB - Operation Tactics Snow Black</title>
+ <title>OTSB - Operation Tactics Snow Black</title>
 
-  <!-- Bootstrap Core CSS -->
-  <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+ <!-- Bootstrap Core CSS -->
+ <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
 
-  <!-- Fonts -->
+ <!-- Fonts -->
 
-  <script src="https://use.fontawesome.com/0ca22a4c02.js"></script>
-  <link href="../css/animate.css" rel="stylesheet" />
-  <!-- Squad theme CSS -->
-  <link href="style.css" rel="stylesheet">
-  <link href="../css/style.css" rel="stylesheet">
-  <link href="../color/default.css" rel="stylesheet">
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-  <script type="text/javascript" src="../fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-  <link rel="stylesheet" type="text/css" href="../fancybox/jquery.fancybox-1.3.4.css" media="screen" />
-  <script type="text/javascript">
-    $(document).ready(function(){
-      $(".ifancybox").fancybox({
-        'width' : '95%',
-        'height' : '95%',
-        'autoScale' : false,
-        'transitionIn' : 'none',
-        'transitionOut' : 'none',
-        'type' : 'iframe'
-      });
+ <script src="https://use.fontawesome.com/0ca22a4c02.js"></script>
+ <link href="../css/animate.css" rel="stylesheet" />
+ <!-- Squad theme CSS -->
+ <link href="style.css" rel="stylesheet">
+ <link href="../css/style.css" rel="stylesheet">
+ <link href="../color/default.css" rel="stylesheet">
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+ <script type="text/javascript" src="../fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+ <link rel="stylesheet" type="text/css" href="../fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+ <script type="text/javascript">
+  $(document).ready(function(){
+    $(".ifancybox").fancybox({
+      'width' : '95%',
+      'height' : '95%',
+      'autoScale' : false,
+      'transitionIn' : 'none',
+      'transitionOut' : 'none',
+      'type' : 'iframe'
     });
-  </script>
+  });
+</script>
 
 
   <!-- =======================================================
@@ -102,19 +150,19 @@ if(!isset($_SESSION["login"]) || !isset($_SESSION["senha"])){
           <li><a href="/#contact">Contato</a></li>
           <li><a href="/galeria">Galeria</a></li>
           <li><a class="ifancybox" href="https://docs.google.com/forms/d/e/1FAIpQLSfKUjGVnpolEFRAuxI2Ati2C28M1Doij9uQtZPqpjyZr0J6ww/viewform">Aliste-se Já</a></li>
-          <!--<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-          <li><a href="#">Example menu</a></li>
-          <li><a href="#">Example menu</a></li>
-          <li><a href="#">Example menu</a></li>
-          </ul>
-        </li-->
-      </ul>
-    </div>
-    <!-- /.navbar-collapse -->
-  </div>
-  <!-- /.container -->
+  <!--<li class="dropdown">
+  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+  <ul class="dropdown-menu">
+  <li><a href="#">Example menu</a></li>
+  <li><a href="#">Example menu</a></li>
+  <li><a href="#">Example menu</a></li>
+  </ul>
+</li-->
+</ul>
+</div>
+<!-- /.navbar-collapse -->
+</div>
+<!-- /.container -->
 </nav>
 
 <!-- Section: intro -->
@@ -167,103 +215,103 @@ if(!isset($_SESSION["login"]) || !isset($_SESSION["senha"])){
                       <section class="block-content panel-margin profile-classic">
                         <span>Atualizar dados cadastrais</span>
                         <table class="width-100">
-                         <tr>
-                          <td colspan="3" style="width: 49%; height: 65px;">
-                            <div class="input-group width-100">
-                              <span class="input-group-addon"><i class="fa fa-envelope" aria-hidden="true"></i></span>
-                              <input id="email" name="email" class="form-control" placeholder="E-mail" type="email" value="<?php echo $email ?>" required="">
-                            </div>
-                          </td>
-                        </tr>
-
-                        <tr>
-                          <td colspan="3" style="width: 49%; height: 65px;">
-                            <div class="input-group width-100" style="display: flex;">
-                              <input id="passwordinput" name="passwordinput" type="password" placeholder="Informe sua nova senha" class="form-control input-md" required="">
-                              <button id="btnPasswordinput" class="input-group-addon fa fa-eye" type="button" style="width: 11%;"></button>
-                            </div>
-                          </td>
-
-                          <td style="width: 2%;"/>
-
-                          <td colspan="3" style="width: 49%;">
-                            <div class="input-group width-100" style="display: flex;">
-                              <input id="passwordinputConfirm" name="passwordinputConfirm" type="password" placeholder="Confirme sua senha" class="form-control input-md" required="">
-                              <button id="btnPasswordinputConfirm" class="input-group-addon fa fa-eye" type="button" style="width: 11%;"></button>
-                            </div>
-                          </td>
-                        </tr>
-                      </table>
-                    </section>
-
-                    <section class="block-content panel-margin profile-classic">
-                      <table class="width-100">
-                        <tr>
-                          <td colspan="3" style="width: 49%; height: 65px;">
-                            <div class="input-group width-100">
-                              <span class="input-group-addon" style="width: 12%;"><i class="fa fa-lock" aria-hidden="true"></i></span>
-                              <input id="passwordatual" name="passwordatual" type="password" placeholder="Informe sua senha atual" class="form-control input-md" required="">
-                            </div>      
-                          </td>
-
-                          <td style="width: 2%;"/>
-
-                          <td colspan="3" style="width: 49%; vertical-align: bottom;">
-                            <div class="form-group">
-                              <!-- Button -->
-                              <div class="col-sm-12 controls">
-                                <button id="btnAtualizar" name="btnAtualizar" type="submit" class="btn btn-primary pull-right"><i class="fa fa-edit"></i> Atualizar</button>
+                          <tr>
+                            <td colspan="3" style="width: 49%; height: 65px;">
+                              <div class="input-group width-100">
+                                <span class="input-group-addon"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                                <input id="email" name="email" class="form-control" placeholder="E-mail" type="email" value="<?php echo $email ?>" required="">
                               </div>
-                            </div>
-                          </td>
-                        </tr>
-                      </table>
+                            </td>
+                          </tr>
+
+                          <tr>
+                            <td colspan="3" style="width: 49%; height: 65px;">
+                              <div class="input-group width-100" style="display: flex;">
+                                <input id="passwordinput" name="passwordinput" type="password" placeholder="Informe sua nova senha" class="form-control input-md">
+                                <button id="btnPasswordinput" class="input-group-addon fa fa-eye" type="button" style="width: 11%;"></button>
+                              </div>
+                            </td>
+
+                            <td style="width: 2%;"/>
+
+                            <td colspan="3" style="width: 49%;">
+                              <div class="input-group width-100" style="display: flex;">
+                                <input id="passwordinputConfirm" name="passwordinputConfirm" type="password" placeholder="Confirme sua senha" class="form-control input-md">
+                                <button id="btnPasswordinputConfirm" class="input-group-addon fa fa-eye" type="button" style="width: 11%;"></button>
+                              </div>
+                            </td>
+                          </tr>
+                        </table>
+                      </section>
+
+                      <section class="block-content panel-margin profile-classic">
+                        <table class="width-100">
+                          <tr>
+                            <td colspan="3" style="width: 49%; height: 65px;">
+                              <div class="input-group width-100">
+                                <span class="input-group-addon" style="width: 12%;"><i class="fa fa-lock" aria-hidden="true"></i></span>
+                                <input id="passwordAtual" name="passwordAtual" type="password" placeholder="Informe sua senha atual" class="form-control input-md" required="">
+                              </div>      
+                            </td>
+
+                            <td style="width: 2%;"/>
+
+                            <td colspan="3" style="width: 49%; vertical-align: bottom;">
+                              <div class="form-group">
+                                <!-- Button -->
+                                <div class="col-sm-12 controls">
+                                  <button id="btnAtualizar" name="btnAtualizar" type="submit" class="btn btn-primary pull-right"><i class="fa fa-edit"></i> Atualizar</button>
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                        </table>
+                      </section>
+                    </form>
+                  </div>
+
+                  <div id="tab1default3" class="tab-pane fade">
+                    <section class="block-content panel-margin profile-classic">
+                      <form id="form" class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data" name="atualizarFoto">
+                        <table class="width-100">
+                          <tr>
+                            <td colspan="3" style="width: 49%; height: 65px;">
+                              <?php  echo "<img class='block-center img-responsive img-circle imgCircle' src='".$path.$dados['foto']."'/>";  ?>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                              <div colspan="3" style="margin-top: 10%;">
+                                <input id="filebutton" name="file-input" onchange="readURL(this);" class="input-file width-100" type="file" accept="image/jpeg">
+                                <span style="font-size: 12px;">*Foto do perfil (150 x 150)</span>
+                              </div>
+                            </td>
+                            <td colspan="3" style="width: 49%; vertical-align: bottom;">
+                              <div class="form-group">
+                                <!-- Button -->
+                                <div class="col-sm-12 controls">
+                                  <button id="btnAtualizarFoto" name="btnAtualizarFoto" type="submit" class="btn btn-primary pull-right"><i class="fa fa-edit"></i> Atualizar</button>
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                        </table>
+                      </form>
                     </section>
-                  </form>
+                  </div>
                 </div>
 
-                <div id="tab1default3" class="tab-pane fade">
-                  <section class="block-content panel-margin profile-classic">
-                    <form id="form" class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data" name="atualizarFoto">
-                      <table class="width-100">
-                        <tr>
-                          <td colspan="3" style="width: 49%; height: 65px;">
-                           <?php  echo "<img class='block-center img-responsive img-circle imgCircle' src='".$path.$dados['foto']."'/>";  ?>
-                         </td>
-                       </tr>
-                       <tr>
-                        <td>
-                         <div colspan="3" style="margin-top: 10%;">
-                          <input id="filebutton" name="file-input" onchange="readURL(this);" class="input-file width-100" type="file" accept="image/jpeg">
-                          <span style="font-size: 12px;">*Foto do perfil (150 x 150)</span>
-                        </div>
-                      </td>
-                      <td colspan="3" style="width: 49%; vertical-align: bottom;">
-                        <div class="form-group">
-                          <!-- Button -->
-                          <div class="col-sm-12 controls">
-                            <button id="btnAtualizarFoto" name="btnAtualizarFoto" type="submit" class="btn btn-primary pull-right"><i class="fa fa-edit"></i> Atualizar</button>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                  </table>
-                </form>
-              </section>
+
+              </div>
+              <div class="tab-pane fade" id="tab2default">Default 2</div>
+              <div class="tab-pane fade" id="tab3default">Default 3</div>
+              <div class="tab-pane fade" id="tab4default">Default 4</div>
             </div>
           </div>
-
-
         </div>
-        <div class="tab-pane fade" id="tab2default">Default 2</div>
-        <div class="tab-pane fade" id="tab3default">Default 3</div>
-        <div class="tab-pane fade" id="tab4default">Default 4</div>
       </div>
     </div>
   </div>
-</div>
-</div>
-</div>
 
 </section>
 
