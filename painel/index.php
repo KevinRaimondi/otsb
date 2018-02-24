89a60a3835a05621f726daba1bd01eadb3aa922e
Fábio Lucena Ribas <fabio.luce@outlook.com>
2018-02-23 21:07:56 -0300

    Iniciando Construção do painel - meu perfil

--- painel/index.php
+++ painel/index.php
@@ -153,6 +153,7 @@ Theme Name: Squadfree
 
   <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
     <div class="container">
+      <?=$msg?>
       <div class="navbar-header page-scroll">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
           <i class="fa fa-bars"></i>
